589f85483c002d30d52823a8beb687aa150e95f5
gitfornikhil <113898562+gitfornikhil@users.noreply.github.com>
2022-11-10 22:04:14 +0530

    Add files via upload

---
+++ Assignment - 1 from Python Basics Assignments..py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment-1 Answers.
+# 
+
+# ### 1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+# 
+# 
+# | * |--> It's an Expression
+# 
+# 'hello' --> It's a Value
+# 
+# -87.8 --> It's a Value 
+# 
+# | - | --> It's an Expression
+# 
+# | / | --> It's an Expression
+# 
+# | + | --> It's an Expression
+# 
+# 6 --> It's a Value
+
+# ### 2. What is the difference between string and variable?
+# String =  A string is just a sequence of characters. A Variable is a store of information.
+# A String is a type of information you would store in a Variable. A String is usually words, enclosed with ""
+# 
+# Eg:- String x ="hello ineurons"
+# X is the Variable, and we declared it as a String, use the single = to assign the text to it.
+# 
+
+# ### 3. Describe three different data types.
+# 
+# * String
+# * Integer,Float,complex
+# * Boolean
+# 
+
+# ### 4. What is an expression made up of? What do all expressions do?
+# 
+# An Expression is made up of Mathematical or Arithmetic operator which performs the activities like subtraction, addition, multiplication, division, remainders etc to produce some value.
+
+# ### 5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+# The Assignment statements show that the Variable 'spam' is equal to '10'. Assignment statement allocate the variable to the value. Whereas, Expression is an operator. It is used to perform an equation.
+
+# ### 6. After running the following code, what does the variable bacon contain?
+# 
+# bacon = 22
+# bacon + 1
+# 
+# ### The variable bacon contains 23.
+
+# In[10]:
+
+
+### 7. What should the values of the following two terms be?
+
+'spam' + 'spamspam' 
+'spam' * 3
+
+### The value of following two terms would be 3 times as 'spamspamspam'
+
+
+# ### 8. Why is eggs a valid variable name while 100 is invalid?
+# 
+# A Variable cannot begin with a number.
+
+# ### 9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+# 
+# * int() for integer
+# * float() for floating-point
+# * str() for string
+
+# ### 10. Why does this expression cause an error? How can you fix it? ' I have eaten ' + 99 + ' burritos.'
+# 
+# * In this expression error would be from concatenation pov. In a string an operator cannot be concatenated with a string.
+# * we can fix by making a little change like ' i have eaten ' + str(99) + ' burritos. '.
+
+# In[ ]:
+
+
+
+
